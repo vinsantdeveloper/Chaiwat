79ce36cf65ed091225fa73d02384d50a952e31b5
“vinsantdeveloper” <suttipongworks@gmail.com>
2019-02-26 00:14:54 +0900

    creating main page (in progress) (Main画像の作成　「途中」)

---
+++ src/QRTrack/QRTrack.Android/obj/Debug/android/src/md5789045af33e42320c4f056babfadbe88/MainActivity.java
@@ -0,0 +1,47 @@
+package md5789045af33e42320c4f056babfadbe88;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("QRTrack.Droid.MainActivity, QRTrack.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("QRTrack.Droid.MainActivity, QRTrack.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
