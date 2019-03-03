37454fe67c1fdd43232cdc4ae3266b971e7e446e
“vinsantdeveloper” <suttipongworks@gmail.com>
2019-03-03 23:25:25 +0900

    creating main page (in progress) ・Entry custom renderer success

---
+++ src/QRTrack/QRTrack.Android/EntryCustomRender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics.Drawables;
+using QRTrack.CustomControl;
+using QRTrack.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(EntryCustomRenderer), typeof(EntryCustomRender))]
+namespace QRTrack.Droid
+{
+    public class EntryCustomRender : EntryRenderer
+    {
+        public EntryCustomRender(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.Background = new ColorDrawable(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
---
+++ src/QRTrack/QRTrack.iOS/EntryCustomRender.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using QRTrack.CustomControl;
+using QRTrack.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(EntryCustomRenderer), typeof(EntryCustomRender))]
+namespace QRTrack.iOS
+{
+    public class EntryCustomRender : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                //Control.BackgroundColor = UIColor.FromRGB(204, 153, 255);
+                Control.BorderStyle = UITextBorderStyle.None;
+            }
+        }
+    }
+}
